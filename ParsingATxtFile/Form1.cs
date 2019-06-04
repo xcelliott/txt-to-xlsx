9b886784a575a569562687b15cdf0d72941d8d52
Cole <51337266+xcelliott@users.noreply.github.com>
2019-06-04 14:43:32 -0400

    Re-submitting last commit

--- ParsingATxtFile/Form1.cs
+++ ParsingATxtFile/Form1.cs
@@ -22,6 +22,7 @@ namespace ParsingATxtFile
         {
             InitializeComponent();
 
+            //Initialize parse button as disabled.
             btnDasButton.Enabled = false;
         }
 
@@ -29,16 +30,17 @@ namespace ParsingATxtFile
 
 
 
-
-
-
+        //Choose file button --
+        //On click opens a file explorer window for the user to navigate to the desired txt file.
         private void Button1_Click(object sender, EventArgs e)
         {
+            //Open file explorer - Only show .txt files.
             OpenFileDialog choofdlog = new OpenFileDialog();
             choofdlog.Filter = "All Files (*.*)|*.*";
             choofdlog.FilterIndex = 1;
             choofdlog.Multiselect = true;
 
+            //Get filepath and verify .txt extension.
             if (choofdlog.ShowDialog() == DialogResult.OK)
             {
                 string sFileName = choofdlog.FileName;
@@ -48,24 +50,29 @@ namespace ParsingATxtFile
                 int indexaroni = lblFilePath.Text.IndexOf(".");
                 string fileTypeVerification = lblFilePath.Text.Substring(indexaroni+1, 3);
 
+                //Verify file type extension and enable Parse File button.
                 if (fileTypeVerification == "txt")
                 {
                     btnDasButton.Enabled = true;
                 }
                 else
                 {
+                    //error message for non-.txt files.
                     btnDasButton.Enabled = false;
                     MessageBox.Show("Please choose a file with a '.txt' extension.");
                 }
             }
         }
 
-
+        //Parse data from .txt file button --
+        //Open an Excel document, export data from .txt (by whitespace), and fill cells of Excel doc by whitespace.
         private void btnDasButton_Click(object sender, EventArgs e)
         {
+            //Init Lists of data from .txt file.
             List<string> theGoods = new List<string>();
             List<string> lines = File.ReadAllLines(lblFilePath.Text).ToList();
 
+            //Open and init Excel doc.
             excel.Application oXL;
             excel.Workbook oWB;
             excel.Worksheet oSheet;
@@ -79,6 +86,7 @@ namespace ParsingATxtFile
                 oWB = (excel.Workbook)oXL.Workbooks.Add("");
                 oSheet = (excel.Worksheet)oWB.ActiveSheet;
 
+                //Generic line/row 1 spacer
                 oSheet.Cells[1, 1] = "TextFileLine#";
                 oSheet.Cells[1, 2] = "LineTextData";
 
@@ -87,13 +95,14 @@ namespace ParsingATxtFile
 
                 for (int i = 1; i <= lines.Count; i++)
                 {
-
+                    //Parses data from the .txt file by line
                     List<string> entries = lines[i].Split(' ').ToList();
                     foreach (var entry in entries)
                     {
+                        //Parses data from lines by whitespace
                         List<string> singleEntry = entry.Split(null).ToList();
-                        
 
+                        //Inserts each data piece by cell
                         for (int o = 0; o < singleEntry.Count; o++)
                         {
                             if (singleEntry[o].StartsWith("0") || singleEntry[o].StartsWith("-") || singleEntry[o].StartsWith("1"))
@@ -103,16 +112,20 @@ namespace ParsingATxtFile
                         }
                     }
 
-
+                    //This is specific to my company documents that include this in each line to space out a description
+                    //Parses description of each line
                     int index01 = lines[i].IndexOf("Y\"");
                     if (index01 > 0)
                     {
+                        //Puts description in Substring of the entire line
                         lines[i] = lines[i].Substring(0, index01 + 1);
                     }
 
+                    //Numbers each line and inserts description data per line
                     oSheet.Cells[1][i + 1] = i;
                     oSheet.Cells[2][i + 1] = lines[i - 1];
 
+                    //This adds the last line of data (line number and description) and ends the loop
                     if (i == lines.Count - 1)
                     {
                         oSheet.Cells[1][lines.Count + 1] = lines.Count;
